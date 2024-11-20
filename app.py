4176460c10ca2bf37d4034bf4af41880e241a521
Harsh Bhardwaj <149497683+rituharsh9436@users.noreply.github.com>
2024-11-21 02:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 import pickle as pk
 
-model = pk.load(open("Heart_disease_model.pkl", "rb"))
+model = joblib.load(open("Heart_disease_model.pkl", "rb"))
 data = pd.read_csv("heart_disease.csv")
 
 st.title("💓 Heart Disease Prediction App")
